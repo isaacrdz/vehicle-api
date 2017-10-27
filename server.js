850159f99c839b472743b3f99da79e7cf6bf1845
isaacrdz <zack_cpp@hotmail.com>
2017-10-27 15:17:05 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,99 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Vehicle = require('./app/models/vehicle');
+
+
+//Configure app for bodyParser
+//lets us grab data from the body of POST
+
+app.use(bodyParser.urlencoded({extend:true}));
+app.use(bodyParser.json());
+
+//Set up port to server to listen on
+var port = process.env.PORT || 3000;
+
+//connect to db
+
+mongoose.connect('mongodb://localhost:27017/codealong');
+
+//API Routes
+
+var router = express.Router();
+
+//Routes will all be prefixed with /api
+
+app.use('/api', router);
+
+//Middlewares
+
+router.use(function(req, res, next){
+  console.log('FYI... There is some processing currently going down')
+  next();
+})
+
+//test Routes
+router.get('/', function(req, res){
+  res.json({message:'Welcome to out API'});
+});
+
+router.route('/vehicles')
+  .post(function(req,res){
+    var vehicle = new Vehicle();
+    vehicle.make = req.body.make;
+    vehicle.model = req.body.model;
+    vehicle.color = req.body.color;
+
+    vehicle.save(function(err){
+      if(err){
+        res.send(err);
+      }
+      res.json({message:'Vehicle was succesfully manafuctured'});
+    });
+  })
+  .get(function(req,res){
+    Vehicle.find(function(err, vehicles){
+      if(err){
+        res.send(err);
+      }
+      res.json(vehicles);
+    });
+  });
+
+  router.route('/vehicle/:vehicle_id')
+  .get(function(req,res){
+    Vehicle.findById(req.params.vehicle_id, function(err, vehicle){
+      if(err){
+        res.send(err);
+      }
+      res.json(vehicle);
+    });
+  });
+
+  router.route('/vehicle/make/:make')
+  .get(function(req,res){
+    Vehicle.find({make:req.params.make}, function(err, vehicle){
+      if(err){
+        res.send(err);
+      }
+      res.json(vehicle);
+    });
+  });
+
+  router.route('/vehicle/color/:color')
+  .get(function(req,res){
+    Vehicle.find({color:req.params.color}, function(err, vehicle){
+      if(err){
+        res.send(err);
+      }
+      res.json(vehicle);
+    });
+  });
+
+
+
+//Fire up server
+app.listen(port,()=>{
+  console.log('Server is running on port ' + port)
+});
